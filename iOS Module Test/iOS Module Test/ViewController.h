939a4bdcdcac468d6978dbf9779a96836aab556e
ChenZhiWen <czwen1993@gmail.com>
2016-04-11 15:41:00 +0800

    init

---
+++ iOS Module Test/iOS Module Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Module Test
+//
+//  Created by ChenZhiWen on 16/4/11.
+//  Copyright © 2016 czwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS Module Test/iOS Module Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS Module Test
+//
+//  Created by ChenZhiWen on 16/4/11.
+//  Copyright © 2016 czwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
